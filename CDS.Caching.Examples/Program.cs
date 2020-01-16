84d4bcf790c47af7a484ea446eda211fc39acb7b
AshV <Hello@AshishViswakarma.com>
2020-01-17 03:11:38 +0530

    Added example code

---
+++ CDS.Caching.Examples/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace CDS.Caching.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Contact : IRecord
+    {
+        [JsonProperty("contactid")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("@odata.etag")]
+        public string ETag { get; set; }
+    }
+
+    public class ContactCache : EntityCache<Contact>
+    {
+        public ContactCache() : base("contact", "contactid", new HttpConfig())
+        {
+
+        }
+    }
+}
